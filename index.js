a31f5e9d6faef9e619863c19361180d4da2f7ff3
sidharth-panwar <sidharth.panwar@gmail.com>
2021-10-01 01:09:02 +0530

    - Added new settings - Removed extract meta (can be turned on in settings) - Added ref to source block

--- index.js
+++ index.js
@@ -4,13 +4,33 @@ function main() {
     const highlightsRegEx = '(==[^=]+==)';
     const boldRegEx = '(\\*\\*[^\\*]+\\*\\*)';
 
-    let settings = {
+    //CHANGE THIS WHEN ADDING NEW SETTINGS
+    const settingsVersion = 'v1';
+
+    //Use only for saving these first time in the plugin settings file
+    let defaultSettings = {
         expr: `${highlightsRegEx}|${boldRegEx}`,
-        summaryTitle: 'Summary'
+        summaryTitle: 'Summary',
+        keepRefs: true,             //Keeps a reference to the source block in this format [*](((uuid)))
+        nested: true,                //Extract from current block and all the child blocks
+        keepMeta: false,              //Remove highlights and bold
+        settingsVersion: settingsVersion
     };
 
-    //Save in settings
-    logseq.updateSettings(settings);
+    //Load plugin settings
+    let pluginSettings = logseq.settings;
+
+    //This is to ensure that in future if new settings are added they are written over to settings file.
+    //This will overwrite user's existing settings though.
+    const shouldUpdateSettings = pluginSettings.settingsVersion != defaultSettings.settingsVersion;
+
+    //If first time, then save default settings
+    if (shouldUpdateSettings) {
+
+        pluginSettings = defaultSettings;
+        logseq.updateSettings(pluginSettings);
+    }
+
 
     //Extraction function which registers Extract as a context menu for a block
     logseq.Editor.registerBlockContextMenuItem(
@@ -19,20 +39,27 @@ function main() {
 
             //Get current block content
             const { content } = await logseq.Editor.getBlock(currentBlock.uuid);
-            const regEx = new RegExp(settings.expr, 'g');
+            const regEx = new RegExp(pluginSettings.expr, 'g');
 
             //Get all extracts that match regex
             const extracts = [...content.matchAll(regEx)];
 
             //Create a summary block below the current block (sibling) - you can change content of this block 
             //from Summary to something else by changing the summaryTitle property in settings
-            var summaryBlock = await logseq.Editor.insertBlock(currentBlock.uuid, settings.summaryTitle, { sibling: true });
+            var summaryBlock = await logseq.Editor.insertBlock(currentBlock.uuid, pluginSettings.summaryTitle, { sibling: true });
 
             //Create the extracts as children blocks of summary block
             extracts.forEach((i) => {
 
-                logseq.Editor.insertBlock(summaryBlock.uuid, i[0], { sibling: false });
+                let content = i[0];
 
+                //Remove == or ** from start and end if keepMeta is false
+                content = pluginSettings.keepMeta ? content : content.slice(2, -2);
+
+                //Keep reference of source block
+                content = pluginSettings.keepRefs ? `${content} [*](((${currentBlock.uuid})))` : content;
+
+                logseq.Editor.insertBlock(summaryBlock.uuid, content, { sibling: false });
             });
         }
     );
